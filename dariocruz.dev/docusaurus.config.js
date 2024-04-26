5416e251d23f4b72c885c51952d5e188024b9ad9
Dario Cruz <rio2012@gmail.com>
2024-04-26 16:12:49 -0400

    Updated info in config.js, exported footer of theme for editing.

--- dariocruz.dev/docusaurus.config.js
+++ dariocruz.dev/docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'DarioCruz.dev',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -129,7 +129,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} DarioCruz.dev. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
