bf3d39ab723ace949262ffc618e59fb502b45899
Dario Cruz <rio2012@gmail.com>
2024-04-29 14:03:03 -0400

    Change tutorial link to portfolio, add linkedin link to right side of navbar.

--- dariocruz.dev/docusaurus.config.js
+++ dariocruz.dev/docusaurus.config.js
@@ -41,17 +41,11 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -81,12 +75,17 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Projects',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
+            position: 'right',
+          },
+          {
+            href: 'https://linkedin.com/',
+            label: 'LinkedIn',
             position: 'right',
           },
         ],
