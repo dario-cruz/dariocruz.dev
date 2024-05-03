6ae69dc3f487ccf28f8209ff4780c29c76efed05
Dario Cruz <rio2012@gmail.com>
2024-05-03 17:13:10 -0400

    Replace place holder blog posts with initial post.

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,8 +3,43 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '5ff'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', '5ba'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', '466'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', 'c3c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', '156'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '88c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', '000'),
+    exact: true
+  },
+  {
     path: '/blog',
-    component: ComponentCreator('/blog', 'e21'),
+    component: ComponentCreator('/blog', 'a5e'),
     exact: true
   },
   {
@@ -13,18 +48,8 @@ export default [
     exact: true
   },
   {
-    path: '/blog/first-blog-post',
-    component: ComponentCreator('/blog/first-blog-post', '89a'),
-    exact: true
-  },
-  {
-    path: '/blog/long-blog-post',
-    component: ComponentCreator('/blog/long-blog-post', '9ad'),
-    exact: true
-  },
-  {
-    path: '/blog/mdx-blog-post',
-    component: ComponentCreator('/blog/mdx-blog-post', 'e9f'),
+    path: '/blog/Site Up and Running',
+    component: ComponentCreator('/blog/Site Up and Running', 'd7d'),
     exact: true
   },
   {
@@ -33,28 +58,28 @@ export default [
     exact: true
   },
   {
-    path: '/blog/tags/docusaurus',
-    component: ComponentCreator('/blog/tags/docusaurus', 'fb9'),
+    path: '/blog/tags/azure',
+    component: ComponentCreator('/blog/tags/azure', 'b73'),
     exact: true
   },
   {
-    path: '/blog/tags/facebook',
-    component: ComponentCreator('/blog/tags/facebook', 'aba'),
+    path: '/blog/tags/cicd',
+    component: ComponentCreator('/blog/tags/cicd', '75b'),
     exact: true
   },
   {
-    path: '/blog/tags/hello',
-    component: ComponentCreator('/blog/tags/hello', '41a'),
+    path: '/blog/tags/react',
+    component: ComponentCreator('/blog/tags/react', 'b8b'),
     exact: true
   },
   {
-    path: '/blog/tags/hola',
-    component: ComponentCreator('/blog/tags/hola', '8e5'),
+    path: '/blog/tags/website',
+    component: ComponentCreator('/blog/tags/website', '461'),
     exact: true
   },
   {
-    path: '/blog/welcome',
-    component: ComponentCreator('/blog/welcome', 'd2b'),
+    path: '/blog/tags/welcome',
+    component: ComponentCreator('/blog/tags/welcome', '5b6'),
     exact: true
   },
   {
---
+++ blog/2024-05-03-SiteUpandRunnning/index.md
@@ -0,0 +1,19 @@
+---
+slug: Site Up and Running
+title: Site Up and Running
+authors: [dcruz]
+tags: [welcome, website, azure, cicd, react]
+---
+
+# Welcome to the Site
+
+Hello all, 👋🏽
+
+My name is Dario and I've been working in the IT Support space for 5 years total. 4 years working for Amazon and 1 year working for The Chefs Warehouse.
+
+Over the course of my experience I have had some amazing hands on time with some great technologies. Checkout the projects section for more info on projects that I have worked on.
+
+# Inception of the Site
+This site came about because I needed a place on the internet to share my work and my musings about technology, AI, and software development.
+
+
