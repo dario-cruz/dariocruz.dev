4bc1e56dae7f36d6d428cbb959886da0c8455c3b
Dario Cruz <rio2012@gmail.com>
2024-07-25 09:11:29 -0400

    Add custom font file, Update CSS to use custom font globally.

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,6 +1,6 @@
 export default [
-  require("/home/dariocru/repos/dariocruz.dev/node_modules/infima/dist/css/default/default.css"),
-  require("/home/dariocru/repos/dariocruz.dev/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/dariocru/repos/dariocruz.dev/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/dariocru/repos/dariocruz.dev/src/css/custom.css"),
+  require("C:\\Users\\DCRUZ\\repos\\dariocruz.dev\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\DCRUZ\\repos\\dariocruz.dev\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\DCRUZ\\repos\\dariocruz.dev\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\DCRUZ\\repos\\dariocruz.dev\\src\\css\\custom.css"),
 ];
